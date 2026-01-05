51a61effc0a43e7c2fb19702e8197aedbf364e42
YousefNabih <yuosefnabih@gmail.com>
2026-01-05 21:08:24 +0200

    Refactor LED functions with GPIO abstraction
    Refactor LED control functions to use GPIO abstraction.

--- OBD-II_Diagnostics/led.c
+++ OBD-II_Diagnostics/led.c
@@ -1,34 +1,52 @@
-#include "led.h"
+#include "leds.h"
+#include "gpio.h"
+
+/* LED Pin Mapping */
+static const struct {
+    GPIO_Port port;
+    uint8 pin;
+} LED_CONFIG[] = {
+    {GPIO_PORTF, PIN1},  /* RED   - PF1 */
+    {GPIO_PORTF, PIN2},  /* BLUE  - PF2 */
+    {GPIO_PORTF, PIN3}   /* GREEN - PF3 */
+};
 
 void LED_Init(void) {
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5;
-    while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R5) == 0);
+    /* Initialize all LEDs as outputs */
+    GPIO_InitPin(LED_CONFIG[LED_RED].port, LED_CONFIG[LED_RED].pin, GPIO_OUTPUT);
+    GPIO_InitPin(LED_CONFIG[LED_BLUE].port, LED_CONFIG[LED_BLUE].pin, GPIO_OUTPUT);
+    GPIO_InitPin(LED_CONFIG[LED_GREEN].port, LED_CONFIG[LED_GREEN].pin, GPIO_OUTPUT);
     
-    GPIO_PORTF_LOCK_R = 0x4C4F434B;
-    GPIO_PORTF_CR_R = 0x1F;
-    
-    GPIO_PORTF_DIR_R |= (RED_LED | BLUE_LED | GREEN_LED);
-    GPIO_PORTF_DEN_R |= (RED_LED | BLUE_LED | GREEN_LED);
-    LED_AllOff();
+    /* Turn off all LEDs */
+    LED_Off(LED_ALL);
 }
 
-void LED_On(uint8_t color) {
-    GPIO_PORTF_DATA_R |= color;
+void LED_On(uint8 color) {
+    if(color == LED_ALL) {
+        GPIO_WritePin(LED_CONFIG[LED_RED].port, LED_CONFIG[LED_RED].pin, GPIO_HIGH);
+        GPIO_WritePin(LED_CONFIG[LED_BLUE].port, LED_CONFIG[LED_BLUE].pin, GPIO_HIGH);
+        GPIO_WritePin(LED_CONFIG[LED_GREEN].port, LED_CONFIG[LED_GREEN].pin, GPIO_HIGH);
+    } else if(color < 3) {
+        GPIO_WritePin(LED_CONFIG[color].port, LED_CONFIG[color].pin, GPIO_HIGH);
+    }
 }
 
-void LED_Off(uint8_t color) {
-    GPIO_PORTF_DATA_R &= ~color;
+void LED_Off(uint8 color) {
+    if(color == LED_ALL) {
+        GPIO_WritePin(LED_CONFIG[LED_RED].port, LED_CONFIG[LED_RED].pin, GPIO_LOW);
+        GPIO_WritePin(LED_CONFIG[LED_BLUE].port, LED_CONFIG[LED_BLUE].pin, GPIO_LOW);
+        GPIO_WritePin(LED_CONFIG[LED_GREEN].port, LED_CONFIG[LED_GREEN].pin, GPIO_LOW);
+    } else if(color < 3) {
+        GPIO_WritePin(LED_CONFIG[color].port, LED_CONFIG[color].pin, GPIO_LOW);
+    }
 }
 
-void LED_Toggle(uint8_t color) {
-    GPIO_PORTF_DATA_R ^= color;
-}
-
-void LED_Set(uint8_t color, uint8_t state) {
-    if(state) LED_On(color);
-    else LED_Off(color);
-}
-
-void LED_AllOff(void) {
-    GPIO_PORTF_DATA_R &= ~(RED_LED | BLUE_LED | GREEN_LED);
+void LED_Toggle(uint8 color) {
+    if(color == LED_ALL) {
+        GPIO_TogglePin(LED_CONFIG[LED_RED].port, LED_CONFIG[LED_RED].pin);
+        GPIO_TogglePin(LED_CONFIG[LED_BLUE].port, LED_CONFIG[LED_BLUE].pin);
+        GPIO_TogglePin(LED_CONFIG[LED_GREEN].port, LED_CONFIG[LED_GREEN].pin);
+    } else if(color < 3) {
+        GPIO_TogglePin(LED_CONFIG[color].port, LED_CONFIG[color].pin);
+    }
 }
